84858242ab3fbe9e2fe0b8bc735bf9ada8b059a3
Robin Breuker <robin02@gmail.com>
2013-07-18 11:50:06 +0200

    Add modules location to ZendInstaller for Zend modules.

--- src/Composer/Installers/ZendInstaller.php
+++ src/Composer/Installers/ZendInstaller.php
@@ -4,6 +4,7 @@ namespace Composer\Installers;
 class ZendInstaller extends BaseInstaller
 {
     protected $locations = array(
+        'module'  => 'application/modules/{$name}',
         'library' => 'library/{$name}/',
         'extra'   => 'extras/library/{$name}/',
     );
