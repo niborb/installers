a16553ffbfbd846c80ebd12b1e308e08ba43ce5f
Robin Breuker <robin02@gmail.com>
2013-07-25 13:32:35 +0200

    Add location for administration part of components.
    Add location for administrator-components.

--- src/Composer/Installers/JoomlaInstaller.php
+++ src/Composer/Installers/JoomlaInstaller.php
@@ -4,6 +4,7 @@ namespace Composer\Installers;
 class JoomlaInstaller extends BaseInstaller
 {
     protected $locations = array(
+        'administrator-component' => 'administrator/components/{$name}/',
         'component'    => 'components/{$name}/',
         'module'       => 'modules/{$name}/',
         'template'     => 'templates/{$name}/',
